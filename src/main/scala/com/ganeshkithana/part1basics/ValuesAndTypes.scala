24cf8696db7dc4280c6c78686841ae1f36cfb8f6
SHG1NH <shg1nh@bosch.com>
2026-05-15 14:00:45 +0530

    Basics - Values and Types

---
+++ src/main/scala/com/ganeshkithana/part1basics/ValuesAndTypes.scala
@@ -0,0 +1,26 @@
+package com.ganeshkithana.part1basics
+
+object ValuesAndTypes {
+  
+  // values
+  val meaningOfLife: Int = 42 //const value -> reassigning is not allowed
+  
+  // type inference
+  val anInteger = 67 // :Int is optional
+  
+  // common types
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val aInt: Int = 34 // 4 bytes
+  val aShort: Short = 232 // 2 bytes
+  val aLong: Long = 44567744546454545L // 8 bytes
+  val aFloat: Float = 2.4f // 4 bytes
+  val aDouble: Double = 3.14 // 8 bytes
+  
+  // String
+  val aString: String = "Ganesh"
+  
+  def main(args: Array[String]): Unit = {
+    
+  }
+}
